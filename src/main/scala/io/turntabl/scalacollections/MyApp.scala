ea4a0f02b0dd2f41d808a2a7e64e185ee022cab7
ankomahene <ankomaheneshadrack@gmail.com>
2019-11-26 15:09:33 +0000

    init

---
+++ src/main/scala/io/turntabl/scalacollections/MyApp.scala
@@ -0,0 +1,32 @@
+package io.turntabl.scalacollections
+
+object MyApp extends App {
+    val favorite = ("shadrack", "Scala")
+    var name, language = favorite
+
+    val countriesCapitals = Map(
+        "Ghana" -> "Accra",
+        "Germany" -> "Berlin",
+        "France" -> "Paris"
+    )
+
+    val c = countriesCapitals.+("India" -> "Delhi", "UK" -> "London")
+    println(c)
+
+    val norway = c.getOrElse("Norway", "Unknown")
+    println(norway)
+
+    val friendsFirstNames = Set("Maxwell", "Justina", "Benjamin", "John", "Cleopatra", "Eva", "Jeniffer")
+    val newFriends= friendsFirstNames.+("Samuel", "Samuel")
+    println(newFriends.count(_ == "Samuel"))
+
+    val names = List("Isaac", "Yaa", "Franklin", "Denis")
+    names foreach(println)
+
+    names foreach(n =>  if(n.length>6) println(n))
+
+    println(names.count(_.length > 6))
+
+
+
+}
